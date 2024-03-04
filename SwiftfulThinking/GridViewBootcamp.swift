41103e313effa807eeb29be591d5d7a265038b72
Bilol Sanatillaev <sanatillayevbilol@gmail.com>
2024-03-04 10:33:45 +0500

    Grid

--- SwiftfulThinking/GridViewBootcamp.swift
+++ SwiftfulThinking/GridViewBootcamp.swift
@@ -9,7 +9,49 @@ import SwiftUI
 
 struct GridViewBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Grid(alignment: .bottom, horizontalSpacing: 10, verticalSpacing: 6) {
+            ForEach(0..<4) {columnIndex in
+                GridRow {
+                    ForEach(0..<4) {rowIndex in
+                        let cellNumber = columnIndex * 4 + rowIndex + 1
+                    
+                        if cellNumber == 7 {
+                            Color.clear
+                                .gridCellUnsizedAxes([.vertical, .horizontal])
+                        }else {
+                            cell(cellNumber)
+                        }
+                    }
+                }
+            }
+        }
+//        Grid {
+//            GridRow {
+//                cell(1)
+//                cell(1)
+//                cell(1)
+//                cell(1)
+//
+//            }
+//            Divider()
+//                .gridCellUnsizedAxes(.horizontal)
+//            GridRow {
+//                cell(2)
+//                cell(2)
+//                cell(2)
+//                cell(2)
+//
+//            }
+//        }
+    }
+    
+    
+    private func cell(_ num: Int)-> some View {
+        Text("\(num)")
+            .font(.largeTitle)
+            .padding()
+            .background{ Color.blue }
     }
 }
 
